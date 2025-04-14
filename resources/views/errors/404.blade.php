95786add05084aae7050227d3f500fc4a64c2d88
Maxym <maxym@Admins-MacBook-Pro.local>
2025-04-14 14:58:48 +0300

    update 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+<head>
+    <link rel="stylesheet" href="{{ asset('css/errors/404.css') }}">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
+    <title>Fishing Store</title>
+</head>
+
+@section('content')
+
+    <div class="main-container">
+        <a href="{{ route('user.main') }}" class="logo-link">
+            <img class="logo-icon" alt="Logo" title="Перейти на головну" src="{{ asset('images/v2/img/logo.svg') }}">
+        </a>
+        <div class="content">
+            <img class="background-img" alt="404 Image" src="{{ asset('images/v2/img/404-img.svg') }}">
+            <div class="description">
+                <h2 class="description-text">Виникла проблема. Сторінку не знайдено</h2>
+                <a class="redirect-to-main" href="{{route('user.main')}}">Повернутися на головну <img class="icon-arrow-right" alt="arrow" src="{{ asset('images/v2/icon/ArrowSmallRight.svg') }}"></a>
+            </div>
+        </div>
+    </div>
+
+@endsection
